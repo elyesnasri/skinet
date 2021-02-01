0b03e6cde4bc08c71f6b0439dd50d9ba3bc704a5
elyesnasri <elyes.nasri@gmail.com>
2021-02-01 19:23:45 +0100

    added corspolicy

--- API/Startup.cs
+++ API/Startup.cs
@@ -28,6 +28,13 @@ namespace API
                 x.UseSqlite(_configuration.GetConnectionString("DefaultConnection")));
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(option =>
+            {
+                option.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:5001");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +47,8 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
