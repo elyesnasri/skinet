65e48585f226df4de3e818eb060fa33717a26c24
elyesnasri <elyes.nasri@gmail.com>
2021-06-14 13:10:43 +0200

    remove bootstrap and font-awsome

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { ShopModule } from './shop/shop.module';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,6 +17,7 @@ import { ShopModule } from './shop/shop.module';
     HttpClientModule,
     CoreModule,
     ShopModule,
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [NavBarComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatSidenavModule],
   exports: [NavBarComponent],
 })
 export class CoreModule {}
